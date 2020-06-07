aa1b0db5153a34149ea43ecf3f03516e443a4496
peter <peter@retep.org>
2020-06-07 16:00:58 +0100

    Try to keep remote server timestamp on war

--- retrieve-jenkins.sh
+++ retrieve-jenkins.sh
@@ -14,4 +14,4 @@ fi
 
 path=${dir}/${version}/jenkins.war
 echo "Retrieving ${path} -> ${dest}"
-curl -sSl -o $dest http://mirrors.jenkins-ci.org/${path}
+curl -sSl --remote-time -o $dest http://mirrors.jenkins-ci.org/${path}
